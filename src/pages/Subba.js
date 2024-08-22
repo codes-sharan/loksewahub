89ef4a8fa572e46494344944928dd3b78d393c73
Sharan Panthi <shpanthi09@gmail.com>
2024-08-22 14:24:50 +0545

    Testing Server connection

--- src/pages/Subba.js
+++ src/pages/Subba.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import logo from "../logo.png";
 
 function Subba() {
   return (
     <div>
       <h3> Welcome to Subba Loksewa preparation. </h3>
+
+      <h3>Logo from frontend </h3>
+
+      <img src={logo} alt="Logo" className="logo" /> <br/> <br/>
+
+<h3> Testing logo from Server</h3>
+      {/* To hosted path */}
+      <img src="https://loksewahub.com/assets/logo.png" alt="Logo" />
+
      </div>
   )
 }
